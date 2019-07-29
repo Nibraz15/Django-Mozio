c42d32ca1b93573413ad83596c702c61a96c3ea7
nibraz <deedat95@gmail.com>
2019-07-29 09:44:39 +0530

    Update

--- src/config/settings.py
+++ src/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^7ysjwt=xk+3me*rp8-*eb^1!s0!6*krb^yy50f^@2n22j%qkv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
